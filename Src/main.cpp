e796836ab3d5c86a3f6328dc82e1571de9673614
Joasia Klasik <asklas@op.pl>
2024-01-17 18:10:35 +0100

    file opening

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,7 +1,23 @@
+// in Build
+// ./scheduler.out ../Data/data.txt
 #include <iostream>
 
 int main(int argc, char * argv [])
 {
-    
+    if(argc != 2)
+    {
+        std::cout << "Niewlasciwa liczba argumentow.\n";
+        return EXIT_FAILURE;
+    }
+
+    char *fileNamePass = argv[1];
+    FILE *fPass = fopen(fileNamePass, "r");
+
+    if (!fPass)
+    {
+        fprintf(stderr, "Blad. Nie otwarto pliku. '%s'\n", fileNamePass);
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
