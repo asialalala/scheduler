ad3791bb61cb8153bde5659b06e95213f03bc222
Joasia Klasik <asklas@op.pl>
2024-02-08 21:38:19 +0100

    gcc correction in main

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,24 +4,24 @@
 #include <iostream>
 #include <string>
 #include <iterator>
-#include <cstring>
+
 #include "../Inc/mine.hpp"
 
-int main(int argc, char * argv [])
+int main(int argc, char *argv[])
 {
-    if(argc != 5)
+    if (argc != 5)
     {
         std::cout << "Niewlasciwa liczba argumentow.\n";
         return EXIT_FAILURE;
     }
 
-    for(int i = 0; i < strlen(argv[1]); i++)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if(!isdigit(argv[1][i]))
+        if (!isdigit(argv[1][i]))
         {
             std::cout << "Niepoprwana ilosc robotow.\n";
             return EXIT_FAILURE;
-        }        
+        }
     }
 
     int robotsNr = atoi(argv[1]);
@@ -29,16 +29,18 @@ int main(int argc, char * argv [])
     std::string fileNamePass = argv[3];
     Mine::Mine mine(robotsNr, quant);
 
-    if(mine.MineInit(argv) == EXIT_FAILURE)
+    if (mine.MineInit(argv) == EXIT_FAILURE)
         return EXIT_FAILURE;
 
-    if(fileNamePass == "-")
+    if (fileNamePass == "-")
     {
-        if(mine.Schedule() == EXIT_FAILURE)
-        return EXIT_FAILURE;
-    }else{
-        if(mine.Schedule(fileNamePass) == EXIT_FAILURE)
-        return EXIT_FAILURE;
+        if (mine.Schedule() == EXIT_FAILURE)
+            return EXIT_FAILURE;
+    }
+    else
+    {
+        if (mine.Schedule(fileNamePass) == EXIT_FAILURE)
+            return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
